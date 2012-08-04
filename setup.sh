779ad59189ba98f25796a88c46e9a0d683da40d6
Ubuntu <ubuntu@ip-10-171-47-100.us-west-1.compute.internal>
2012-08-04 19:06:30 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
 export SFSHOME=${PWD}
+export CLASSPATH=".:lib/DBPool-5.0.jar:lib/commons-logging-1.1.jar:lib/json-simple-1.1.1.jar:lib/log4j.jar:lib/memcached/:lib/mongo-2.8.0.jar:lib/mysql-connector-java-5.1.20-bin.jar:lib/simple/"
+javac -classpath $CLASSPATH lib/memcached/com/meetup/memcached/*.java
